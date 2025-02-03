af6add4ab0fe2e8f86d8772f563658f228d282c6
Thang0226 <thang.nd0226@gmail.com>
2025-02-03 14:31:02 +0700

    get todo list from api by axios & show loading screen

--- src/App.js
+++ src/App.js
@@ -1,52 +1,81 @@
-import './App.css';
-import {Component} from "react";
+import "./App.css";
+import { Component } from "react";
+import axios from "axios";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       list: [],
-      item: ""
+      item: "",
+      loading: false,
     };
   }
 
+  componentDidMount = async () => {
+    this.setState({ loading: true });
+    await axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        this.setState({
+          list: res.data,
+        });
+      })
+      .finally(() => {
+        this.setState({ loading: false });
+      });
+  };
+
   handleChange = (event) => {
     this.setState({
-      item: event.target.value
-    })
-  }
+      item: event.target.value,
+    });
+  };
 
   handleAddItem = () => {
     if (this.state.item === "") {
       return;
     }
+    let newItem = {
+      id: this.state.list.length + 1,
+      title: this.state.item,
+    };
     this.setState({
-      list: [...this.state.list, this.state.item],
-      item: ""
-    })
-  }
+      list: [...this.state.list, newItem],
+      item: "",
+    });
+  };
 
   render() {
+    const { loading, list } = this.state;
+    if (loading) {
+      return <h2>Loading...</h2>;
+    }
     return (
-      <div style={{textAlign: 'center'}}>
+      <div style={{ textAlign: "center" }}>
         <h1>To-do List</h1>
-        <input type="text" value={this.state.item} onChange={this.handleChange} />
+        <input
+          type="text"
+          value={this.state.item}
+          onChange={this.handleChange}
+        />
         <button onClick={this.handleAddItem}>Add</button>
-        <br/><br/>
+        <br />
+        <br />
         <table>
           <thead>
-          <tr>
-            <th className="index">No.</th>
-            <th>To-do</th>
-          </tr>
+            <tr>
+              <th className="index">ID</th>
+              <th>To-do</th>
+            </tr>
           </thead>
           <tbody>
-          {this.state.list.map((item, index) => (
+            {list.map((item) => (
               <tr>
-                <td className="index">{index+1}</td>
-                <td>{item}</td>
+                <td className="index">{item.id}</td>
+                <td>{item.title}</td>
               </tr>
-          ))}
+            ))}
           </tbody>
         </table>
       </div>
